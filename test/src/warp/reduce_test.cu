6a758eeb024f7e001be43f0021bde86e4eb2893a
Georgii Evtushenko <georgii.evtushenko@rfdyn.ru>
2020-06-14 21:30:18 +0300

    Fix cc 1.1 build

--- test/src/warp/reduce_test.cu
+++ test/src/warp/reduce_test.cu
@@ -73,6 +73,22 @@ void perform_reduce_test ()
 
 TEST(warp_reduce, int) { perform_reduce_test<int> (); }
 
+#if defined(__CUDACC__) // NVCC
+  #define CULIB_ALIGN(n) __align__(n)
+#elif defined(__GNUC__) // GCC
+  #define CULIB_ALIGN(n) __attribute__((aligned(n)))
+#elif defined(_MSC_VER) // MSVC
+  #define CULIB_ALIGN(n) __declspec(align(n))
+#else
+  #error "Please provide a definition for MY_ALIGN macro for your host compiler!"
+#endif
+
+template <typename data_type>
+struct CULIB_ALIGN(sizeof(data_type)) placeholder
+{
+  char place[sizeof (data_type) / sizeof (char)];
+};
+
 template <typename data_type>
 class user_reducer
 {
@@ -80,7 +96,7 @@ public:
   __device__ void operator () (data_type const * const in, data_type * const out)
   {
     constexpr size_t warp_size = 32;
-    __shared__ char cache[warp_size * sizeof (user_type)];
+    __shared__ placeholder<data_type> cache[warp_size];
     culib::warp::reduce<data_type> reduce (reinterpret_cast<user_type *> (cache));
     out[threadIdx.x] = reduce (in[threadIdx.x]);
   }
