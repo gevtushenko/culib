a158951c440066b50319a408571307a129dda072
Georgii Evtushenko <georgii.evtushenko@rfdyn.ru>
2020-07-01 14:45:38 +0300

    Use template cache variable

--- include/culib/device/memory/const_resizable_array.cuh
+++ include/culib/device/memory/const_resizable_array.cuh
@@ -8,25 +8,18 @@
 #ifndef CULIB_CONST_RESIZABLE_ARRAY_CUH
 #define CULIB_CONST_RESIZABLE_ARRAY_CUH
 
-#ifndef CULIB_CONST_STORAGE_SIZE
-#define CULIB_CONST_STORAGE_SIZE 1024
-#endif
-
-#ifndef CULIB_CONST_STORAGE_TYPE
-#define CULIB_CONST_STORAGE_TYPE char
-#endif
-
 namespace culib
 {
 namespace device
 {
 
-__constant__ CULIB_CONST_STORAGE_TYPE cache[CULIB_CONST_STORAGE_SIZE];
+template <typename data_type, unsigned int size>
+__constant__ data_type cache[size];
 
-template<typename data_type>
+template<typename data_type, unsigned int const_size>
 class const_resizable_array;
 
-template<typename data_type>
+template<typename data_type, unsigned int const_size = 1024>
 class const_resizeable_array_accessor
 {
   data_type *ptr;
@@ -35,25 +28,21 @@ public:
 
   __device__ data_type operator[] (unsigned int idx)
   {
-    constexpr unsigned int elements_in_cache = CULIB_CONST_STORAGE_SIZE * sizeof (CULIB_CONST_STORAGE_TYPE)
-                                             / sizeof (data_type);
-    if (idx < elements_in_cache)
-      return cache[idx];
+    if (idx < const_size)
+      return cache<data_type, const_size>[idx];
     else
-      return ptr[idx - elements_in_cache];
+      return ptr[idx - const_size];
   }
 
 private:
   __host__ const_resizeable_array_accessor (data_type *ptr_arg) : ptr (ptr_arg) { }
 
-  friend class const_resizable_array<data_type>;
+  friend class const_resizable_array<data_type, const_size>;
 };
 
-template<typename data_type>
+template<typename data_type, unsigned int const_size = 1024>
 class const_resizable_array
 {
-  const unsigned int elements_in_cache = CULIB_CONST_STORAGE_SIZE * sizeof (CULIB_CONST_STORAGE_TYPE)
-                                       / sizeof (data_type);
   resizable_array<data_type> dynamic_memory;
 
 public:
@@ -65,21 +54,21 @@ public:
 
   bool resize (unsigned int size)
   {
-    if (size > elements_in_cache)
-      return dynamic_memory.resize (size - elements_in_cache);
+    if (size > const_size)
+      return dynamic_memory.resize (size - const_size);
     return true;
   }
 
   void send_n (const data_type *host_data, unsigned int n)
   {
-    if (elements_in_cache)
-      cudaMemcpyToSymbol (cache, host_data, sizeof (data_type) * std::min (elements_in_cache, n));
+    if (const_size)
+      cudaMemcpyToSymbol (cache<data_type, const_size>, host_data, sizeof (data_type) * std::min (const_size, n));
 
-    if (n > elements_in_cache)
-      culib::device::send_n (host_data + elements_in_cache, n - elements_in_cache, dynamic_memory.get ());
+    if (n > const_size)
+      culib::device::send_n (host_data + const_size, n - const_size, dynamic_memory.get ());
   }
 
-  const_resizeable_array_accessor<data_type> get_accessor ()
+  const_resizeable_array_accessor<data_type, const_size> get_accessor ()
   {
     return { dynamic_memory.get () };
   }
