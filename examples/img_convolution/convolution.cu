c9d372cea3e14f727f4158f59175fa9bdc4e006f
Georgii Evtushenko <georgii.evtushenko@rfdyn.ru>
2020-07-02 22:49:16 +0300

    Work with float convolution

--- examples/img_convolution/convolution.cu
+++ examples/img_convolution/convolution.cu
@@ -27,16 +27,16 @@ class result_class
 {
 public:
   float elapsed {};
-  std::unique_ptr<unsigned char[]> data;
+  std::unique_ptr<float[]> data;
 };
 
 template <typename weights_type>
 __global__ void convolution_constant_kernel (
   int width,
   int height,
-  const unsigned char *data,
+  const float *data,
   const weights_type weights,
-  unsigned char *result
+  float *result
   )
 {
   const int row = threadIdx.y + blockDim.y * blockIdx.y;
@@ -51,42 +51,41 @@ __global__ void convolution_constant_kernel (
     }
 
   // First row
-  const char res_0 = data[(row - 1) * width + col - 1] * weights[0 * 3 + 0];
-  const char res_1 = data[(row - 1) * width + col + 0] * weights[0 * 3 + 1];
-  const char res_2 = data[(row - 1) * width + col + 1] * weights[0 * 3 + 2];
-  const char first_row_sum = res_0 + res_1 + res_2;
+  const float res_0 = data[(row - 1) * width + col - 1] * weights[0 * 3 + 0];
+  const float res_1 = data[(row - 1) * width + col + 0] * weights[0 * 3 + 1];
+  const float res_2 = data[(row - 1) * width + col + 1] * weights[0 * 3 + 2];
+  const float first_row_sum = res_0 + res_1 + res_2;
 
   // Second row
-  const char res_3 = data[(row + 0) * width + col - 1] * weights[1 * 3 + 0];
-  const char res_4 = data[(row + 0) * width + col + 0] * weights[1 * 3 + 1];
-  const char res_5 = data[(row + 0) * width + col + 1] * weights[1 * 3 + 2];
-  const char second_row_sum = res_3 + res_4 + res_5;
+  const float res_3 = data[(row + 0) * width + col - 1] * weights[1 * 3 + 0];
+  const float res_4 = data[(row + 0) * width + col + 0] * weights[1 * 3 + 1];
+  const float res_5 = data[(row + 0) * width + col + 1] * weights[1 * 3 + 2];
+  const float second_row_sum = res_3 + res_4 + res_5;
 
   // Third row
-  const char res_6 = data[(row + 1) * width + col - 1] * weights[2 * 3 + 0];
-  const char res_7 = data[(row + 1) * width + col + 0] * weights[2 * 3 + 1];
-  const char res_8 = data[(row + 1) * width + col + 1] * weights[2 * 3 + 2];
-  const char third_row_sum = res_6 + res_7 + res_8;
+  const float res_6 = data[(row + 1) * width + col - 1] * weights[2 * 3 + 0];
+  const float res_7 = data[(row + 1) * width + col + 0] * weights[2 * 3 + 1];
+  const float res_8 = data[(row + 1) * width + col + 1] * weights[2 * 3 + 2];
+  const float third_row_sum = res_6 + res_7 + res_8;
 
   result[row * width + col] = first_row_sum + second_row_sum + third_row_sum;
 }
 
-result_class convolution_constant (const img_class *img)
+template <typename weights_type>
+result_class convolution_constant (
+  weights_type weights,
+  const img_class *img)
 {
   result_class result;
 
-  culib::device::resizable_array<unsigned char> device_img (img->pixels_count);
-  culib::device::resizable_array<unsigned char> device_result (img->pixels_count);
-  culib::device::send_n (img->data.get (), img->pixels_count, device_img.get ());
+  const unsigned char *cpu_char_data = img->data.get ();
+  std::unique_ptr<float[]> host_img (new float[img->pixels_count]);
+  for (unsigned int i = 0; i < img->pixels_count; i++)
+    host_img[i] = cpu_char_data[i];
 
-  constexpr int weights_size = 9;
-  const unsigned char host_weights[weights_size] =
-    { 2, 5, 2,
-      5, 9, 5,
-      2, 5, 2 };
-
-  culib::device::const_resizable_array<unsigned char, weights_size> device_weights (weights_size);
-  device_weights.send_n (host_weights, weights_size);
+  culib::device::resizable_array<float> device_img (img->pixels_count);
+  culib::device::resizable_array<float> device_result (img->pixels_count);
+  culib::device::send_n (host_img.get (), img->pixels_count, device_img.get ());
 
   const unsigned int threads_per_dim = 16;
   const unsigned int blocks_per_x = (img->width + threads_per_dim - 1) / threads_per_dim;
@@ -100,7 +99,7 @@ result_class convolution_constant (const img_class *img)
 
   cudaEventRecord (begin);
   convolution_constant_kernel<<<grid_size, thread_block_size>>> (
-    img->width, img->height, device_img.get (), device_weights.get_accessor (), device_result.get ());
+    img->width, img->height, device_img.get (), weights, device_result.get ());
   cudaEventRecord (end);
   cudaEventSynchronize (end);
 
@@ -110,52 +109,7 @@ result_class convolution_constant (const img_class *img)
   cudaEventDestroy (begin);
   cudaEventDestroy (end);
 
-  result.data.reset (new unsigned char[img->pixels_count]);
-  culib::device::recv_n (device_result.get (), img->pixels_count, result.data.get ());
-
-  return result;
-}
-
-result_class convolution (const img_class *img)
-{
-  result_class result;
-
-  culib::device::resizable_array<unsigned char> device_img (img->pixels_count);
-  culib::device::resizable_array<unsigned char> device_result (img->pixels_count);
-  culib::device::send_n (img->data.get (), img->pixels_count, device_img.get ());
-
-  constexpr int weights_size = 9;
-  const unsigned char host_weights[weights_size] =
-    { 2, 5, 2,
-      5, 9, 5,
-      2, 5, 2 };
-
-  culib::device::resizable_array<unsigned char> device_weights (weights_size);
-  culib::device::send_n (host_weights, weights_size, device_weights.get ());
-
-  const unsigned int threads_per_dim = 16;
-  const unsigned int blocks_per_x = (img->width + threads_per_dim - 1) / threads_per_dim;
-  const unsigned int blocks_per_y = (img->height + threads_per_dim - 1) / threads_per_dim;
-  dim3 thread_block_size (threads_per_dim, threads_per_dim);
-  dim3 grid_size (blocks_per_x, blocks_per_y);
-
-  cudaEvent_t begin, end;
-  cudaEventCreate (&begin);
-  cudaEventCreate (&end);
-
-  cudaEventRecord (begin);
-  convolution_constant_kernel<<<grid_size, thread_block_size>>> (
-    img->width, img->height, device_img.get (), device_weights.get (), device_result.get ());
-  cudaEventRecord (end);
-  cudaEventSynchronize (end);
-
-  cudaEventElapsedTime (&result.elapsed, begin, end);
-  result.elapsed /= 1000.0f;
-
-  cudaEventDestroy (begin);
-  cudaEventDestroy (end);
-
-  result.data.reset (new unsigned char[img->pixels_count]);
+  result.data.reset (new float[img->pixels_count]);
   culib::device::recv_n (device_result.get (), img->pixels_count, result.data.get ());
 
   return result;
@@ -164,22 +118,27 @@ result_class convolution (const img_class *img)
 result_class convolution_cpu_naive (const img_class *img)
 {
   result_class result;
-  result.data.reset (new unsigned char[img->pixels_count]);
+  result.data.reset (new float[img->pixels_count]);
+
+  const unsigned char *cpu_char_data = img->data.get ();
+  std::unique_ptr<float[]> host_img (new float[img->pixels_count]);
+  for (unsigned int i = 0; i < img->pixels_count; i++)
+    host_img[i] = cpu_char_data[i];
 
   const unsigned int height = img->height;
   const unsigned int width = img->width;
 
-  unsigned char *res = result.data.get ();
+  float *res = result.data.get ();
 
   auto begin = std::chrono::high_resolution_clock::now ();
 
   constexpr int weights_size = 9;
-  const unsigned char weights[weights_size] =
+  const float weights[weights_size] =
     { 2, 5, 2,
       5, 9, 5,
       2, 5, 2 };
 
-  const unsigned char *inp = img->data.get ();
+  const float *inp = host_img.get ();
 
   for (unsigned int j = 0; j < width; j++)
     res[j] = {};
@@ -191,20 +150,20 @@ result_class convolution_cpu_naive (const img_class *img)
 
       for (unsigned int j = 1; j < width - 1; j++)
         {
-          const unsigned int r1 = inp[(i - 1) * width + j - 1] * weights[0 * 3 + 0];
-          const unsigned int r2 = inp[(i - 1) * width + j + 0] * weights[0 * 3 + 1];
-          const unsigned int r3 = inp[(i - 1) * width + j + 1] * weights[0 * 3 + 2];
-          const unsigned int first_row_sum = r1 + r2 + r3;
+          const float r1 = inp[(i - 1) * width + j - 1] * weights[0 * 3 + 0];
+          const float r2 = inp[(i - 1) * width + j + 0] * weights[0 * 3 + 1];
+          const float r3 = inp[(i - 1) * width + j + 1] * weights[0 * 3 + 2];
+          const float first_row_sum = r1 + r2 + r3;
 
-          const unsigned int r4 = inp[(i + 0) * width + j - 1] * weights[1 * 3 + 0];
-          const unsigned int r5 = inp[(i + 0) * width + j + 0] * weights[1 * 3 + 1];
-          const unsigned int r6 = inp[(i + 0) * width + j + 1] * weights[1 * 3 + 2];
-          const unsigned int second_row_sum = r4 + r5 + r6;
+          const float r4 = inp[(i + 0) * width + j - 1] * weights[1 * 3 + 0];
+          const float r5 = inp[(i + 0) * width + j + 0] * weights[1 * 3 + 1];
+          const float r6 = inp[(i + 0) * width + j + 1] * weights[1 * 3 + 2];
+          const float second_row_sum = r4 + r5 + r6;
 
-          const unsigned int r7 = inp[(i + 1) * width + j - 1] * weights[2 * 3 + 0];
-          const unsigned int r8 = inp[(i + 1) * width + j + 0] * weights[2 * 3 + 1];
-          const unsigned int r9 = inp[(i + 1) * width + j + 1] * weights[2 * 3 + 2];
-          const unsigned int third_row_sum = r7 + r8 + r9;
+          const float r7 = inp[(i + 1) * width + j - 1] * weights[2 * 3 + 0];
+          const float r8 = inp[(i + 1) * width + j + 0] * weights[2 * 3 + 1];
+          const float r9 = inp[(i + 1) * width + j + 1] * weights[2 * 3 + 2];
+          const float third_row_sum = r7 + r8 + r9;
 
           res[j] = first_row_sum + second_row_sum + third_row_sum;
         }
@@ -252,16 +211,51 @@ int main (int argc, char *argv[])
 
   result_class result = [&] ()
   {
+    constexpr int weights_size = 9;
+    const float host_weights[weights_size] =
+      { 2, 5, 2,
+        5, 9, 5,
+        2, 5, 2 };
+
     if (mode == calculation_mode::cpu)
-      return convolution_cpu_naive (img.get ());
+      {
+        return convolution_cpu_naive (img.get ());
+      }
     if (mode == calculation_mode::gpu)
-      return convolution (img.get ());
+      {
+        culib::device::resizable_array<float> device_weights (weights_size);
+        culib::device::send_n (host_weights, weights_size, device_weights.get ());
+        return convolution_constant (device_weights.get (), img.get ());
+      }
     if (mode == calculation_mode::gpu_constant)
-      return convolution_constant (img.get ());
+      {
+        culib::device::const_resizable_array<float, weights_size> device_weights (weights_size);
+        device_weights.send_n (host_weights, weights_size);
+        return convolution_constant (device_weights.get_accessor (), img.get ());
+      }
     return result_class {};
   } ();
 
   std::cout << "Complete in " << result.elapsed << "s\n";
-  write_png_file (result.data.get (), img->width, img->height, "result.png");
+
+  float maximum = 0.0;
+  float minimum = std::numeric_limits<float>::max ();
+  const float *result_data = result.data.get ();
+  for (unsigned int i = 0; i < img->pixels_count; i++)
+  {
+    maximum = std::max (maximum, result_data[i]);
+    minimum = std::min (minimum, result_data[i]);
+  }
+
+  std::unique_ptr<unsigned char[]> char_result (new unsigned char[img->pixels_count]);
+  unsigned char *char_data = char_result.get ();
+
+  for (unsigned int i = 0; i < img->pixels_count; i++)
+  {
+    const float normilized_value = (result_data[i] - minimum) / (maximum - minimum) * 255.0f;
+    char_data[i] = normilized_value;
+  }
+
+  write_png_file (char_data, img->width, img->height, "result.png");
   return 0;
 }
